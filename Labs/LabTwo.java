554e529c05e46e16a1c6ce2ec4d4e0c482cb77d5
The Dylans <contact@dylanmadisetti.com>
2014-08-26 10:04:53 -0400

    Please kill me

---
+++ Labs/LabTwo.java
@@ -0,0 +1,16 @@
+// This is a class
+public class LabTwo{
+
+	// Code is called from here
+	public static void main(String args[]){
+
+		// This prints stuff.
+		System.out.println("Greetings");
+		System.out.println("I am Dylan Madisetti");
+		System.out.println("I happen to be an armadillo person");
+		System.out.println("I like cheese.");
+
+		// Done.
+	}
+
+}
