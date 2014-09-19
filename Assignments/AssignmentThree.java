fa43ae337ab33b3fc1c9aa2d634d8de1d7696b32
Dylan Madisetti <contact@dylanmadisetti.com>
2014-09-19 17:43:08 -0400

    Added notes

--- Assignments/AssignmentThree.java
+++ Assignments/AssignmentThree.java
@@ -48,3 +48,66 @@ public class AssignmentThree{
         new AssignmentThree();
     }
 }
+
+// Notes on this:
+// I was hoping for tail recursion optimization, but java doesn't do that.
+// Here's a dissection from javap: 
+//
+//  private boolean recurse(int, int, int);
+//    Code:
+//       0: iload_1       
+//       1: iconst_3      
+//       2: if_icmpgt     16
+//       5: iload_3       
+//       6: iload_3       
+//       7: imul          
+//       8: sipush        200
+//      11: iload_2       
+//      12: isub          
+//      13: if_icmple     18
+//      16: iconst_0      
+//      17: ireturn       
+//      18: iload_3       
+//      19: iload_3       
+//      20: imul          
+//      21: istore        4
+//      23: aload_0       
+//      24: getfield      #4                  // Field answers:[I
+//      27: iload_1       
+//      28: iload_3       
+//      29: iastore       
+//      30: iload_2       
+//      31: iload         4
+//      33: iadd          
+//      34: sipush        200
+//      37: if_icmpeq     69
+//      40: aload_0       
+//      41: iload_1       
+//      42: iconst_1      
+//      43: iadd          
+//      44: iload_2       
+//      45: iload         4
+//      47: iadd          
+//      48: iload_3       
+//      49: iconst_1      
+//      50: iadd          
+//      51: invokespecial #5                  // Method recurse:(III)Z
+//      54: ifne          69
+//      57: aload_0       
+//      58: iload_1       
+//      59: iload_2       
+//      60: iload_3       
+//      61: iconst_1      
+//      62: iadd          
+//      63: invokespecial #5                  // Method recurse:(III)Z
+//      66: ifeq          73
+//      69: iconst_1      
+//      70: goto          74
+//      73: iconst_0      
+//      74: ireturn       
+//
+// Ideally instuctions 51 and 63 should be gotos instead of pushing another function on the stack
+// Futher reading shows that java purposefully doesn't tail recurse. Why? I don't know, reading 
+// online shows that Oracle doesn't care enough to do it. However, it's what's keeping me from 
+// setting TARGET = 1000000 and not worrying about a stackoverflow. It should be coming in some 
+// version of 8
