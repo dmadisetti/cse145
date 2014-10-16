df2fc2b2563be73582b79eec67606ad8dd05f666
Dylan Madisetti <contact@dylanmadisetti.com>
2014-10-16 10:15:58 -0400

    Comments I guess

--- Labs/LabThirteen/Game.java
+++ Labs/LabThirteen/Game.java
@@ -16,70 +16,70 @@ public class Game{
     private boolean over = false; // Can't be over before we've begun
 
     // Regex for input
-	private Matcher match;
+    private Matcher match;
     private final String OVER = "over";
     private final String REGEX = "^(over|(a|b) ([0-9]*))$";
     private final Pattern PATTERN = Pattern.compile(REGEX);
 
     // Constructors are constructorful
     public Game(){
-    	System.out.println("Welcome to a game of basket ball");
-    	setTeam(0); // The instructions are wrong. Teams go in the constructor. 
-    	setTeam(1); // There shouldn't be a game without teams
+        System.out.println("Welcome to a game of basket ball");
+        setTeam(0); // The instructions are wrong. Teams go in the constructor. 
+        setTeam(1); // There shouldn't be a game without teams
     }
 
     // Should not be capitalized
     public void Start(){
-    	while(!over){
-    		System.out.println("\nEnter the team (either a or b) and a score.  Or enter 'over' to end the game");
+        while(!over){
+            System.out.println("\nEnter the team (either a or b) and a score.  Or enter 'over' to end the game");
 
-	        //  Let's check, match and parse against the regex
-    		match = PATTERN.matcher(scanner.nextLine());
-	        if (!match.matches()){
-	    		System.out.println("Invalid format!"); // Woops. Guess it's not valid format.
-	    		continue;
-	        }
+            //  Let's check, match and parse against the regex
+            match = PATTERN.matcher(scanner.nextLine());
+            if (!match.matches()){
+                System.out.println("Invalid format!"); // Woops. Guess it's not valid format.
+                continue;
+            }
 
-	        // Is it over yet?
-	        if(match.group(1).equals("over")){
-	        	over = true;
-				System.out.println("The game is over");
-			// If no over attempt to set score
-	        }else if(!teams[match.group(2).equals("a") ? 0 : 1].score(Integer.parseInt(match.group(3)))){ // I should have probably broken this up, but golf is kinda fun. You should see me with anonymous functions in python
-		       	System.out.println("Invalid Score!"); // score returned false
-		    }
+            // Is it over yet?
+            if(match.group(1).equals("over")){
+                over = true;
+                System.out.println("The game is over");
+            // If no over attempt to set score
+            }else if(!teams[match.group(2).equals("a") ? 0 : 1].score(Integer.parseInt(match.group(3)))){ // I should have probably broken this up, but golf is kinda fun. You should see me with anonymous functions in python
+                System.out.println("Invalid Score!"); // score returned false
+            }
 
-	        // How are we doing?
-	        report();
-    	}
+            // How are we doing?
+            report();
+        }
     }
 
     private void report(){
-    	// Spit out junk
+        // Spit out junk
         regurgitate(teams[0]);
-    	regurgitate(teams[1]);
+        regurgitate(teams[1]);
 
-    	// Tied or do we have a winnr?
-    	if(teams[0].equals(teams[1])){
-    		System.out.println("They are tied!!!");
-    	}else{
-	    	System.out.println(compare(teams[0],teams[1]).toString() + " is winning.");
-		}
+        // Tied or do we have a winnr?
+        if(teams[0].equals(teams[1])){
+            System.out.println("They are tied!!!");
+        }else{
+            System.out.println(compare(teams[0],teams[1]).toString() + " is winning.");
+        }
     }
 
     // Just print out stuff
     private void regurgitate(Team x){
-	    System.out.println(x.toString() + " has " + x.getPoints() + " Points");
+        System.out.println(x.toString() + " has " + x.getPoints() + " Points");
     }
 
     // Which is bigger?
     private Team compare(Team a, Team b){
-    	return a.getPoints() > b.getPoints()? a : b;
+        return a.getPoints() > b.getPoints()? a : b;
     }
 
     // Set teams
     private void setTeam(int index){
-    	System.out.println("Enter the name for team " + (index+1));
-    	teams[index] = new Team(scanner.nextLine());
+        System.out.println("Enter the name for team " + (index+1));
+        teams[index] = new Team(scanner.nextLine());
     }
 }
